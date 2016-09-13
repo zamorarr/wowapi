50b967c4536b686f719171a5d96433bf80dfa1ce
Robert Zamora <zamora.rr@gmail.com>
2016-09-12 23:37:25 -0400

    refactor api calls

--- R/character_profile.R
+++ R/character_profile.R
@@ -1,120 +1,116 @@
-character_profile <- function(realm, characterName, fields) {
-  baseurl <- "https://us.api.battle.net"
+character_profile_api <- function(realm, characterName, fields) {
   path <- c("wow", "character", realm, characterName)
+  query <- list(fields = fields)
 
-  query <- list(locale = "en_US",
-                apikey = Sys.getenv("WOW_API_KEY"),
-                fields = fields)
-
-  httr::GET(baseurl, path = path,  query = query)
+  wow_api(path, query)
 }
 
 make_cp_query <- function(fields) {
-  function(realm, characterName) character_profile(realm, characterName, fields)
+  function(realm, characterName) character_profile_api(realm, characterName, fields)
 }
 
-#' @title  Character Profile
+#' @title  Character Profile API
 #'
 #' @param realm name of the WoW Realm
 #' @param characterName name of the character you want info on
 #'
 #' @details The Character Profile API is the primary way to access character information.
 #' \describe{
-#'  \item{get_achievements}{A map of achievement data including completion timestamps and criteria information}
-#'  \item{get_appearance}{A map of a character's appearance settings such as which face texture they've selected and whether or not a healm is visible}
-#'  \item{get_feed}{The activity feed of the character}
-#'  \item{get_guild}{A summary of the guild that the character belongs to. If the character does not belong to a guild and this field is requested, this field will not be exposed}
-#'  \item{get_hunter_pets}{A list of all of the combat pets obtained by the character}
-#'  \item{get_items}{A list of items equipped by the character. Use of this field will also include the average item level and average item level equipped for the character}
-#'  \item{get_mounts}{A list of all of the mounts obtained by the character}
-#'  \item{get_pets}{A list of the battle pets obtained by the character}
-#'  \item{get_pet_slots}{Data about the current battle pet slots on this characters account}
-#'  \item{get_professions}{A list of the character's professions. Does not include class professions}
-#'  \item{get_progression}{A list of raids and bosses indicating raid progression and completeness}
-#'  \item{get_pvp}{A map of pvp information including arena team membership and rated battlegrounds information}
-#'  \item{get_quests}{A list of quests completed by the character}
-#'  \item{get_reputation}{A list of the factions that the character has an associated reputation with}
-#'  \item{get_statistics}{A map of character statistics}
-#'  \item{get_stats}{A map of character attributes and stats}
-#'  \item{get_character_talents}{A list of talent structures}
-#'  \item{get_titles}{A list of the titles obtained by the character including the currently selected title}
-#'  \item{get_audit}{Raw character audit data that powers the character audit on the game site}
+#'  \item{character_achievements}{A map of achievement data including completion timestamps and criteria information}
+#'  \item{character_appearance}{A map of a character's appearance settings such as which face texture they've selected and whether or not a healm is visible}
+#'  \item{character_feed}{The activity feed of the character}
+#'  \item{character_guild}{A summary of the guild that the character belongs to. If the character does not belong to a guild and this field is requested, this field will not be exposed}
+#'  \item{character_hunter_pets}{A list of all of the combat pets obtained by the character}
+#'  \item{character_items}{A list of items equipped by the character. Use of this field will also include the average item level and average item level equipped for the character}
+#'  \item{character_mounts}{A list of all of the mounts obtained by the character}
+#'  \item{character_pets}{A list of the battle pets obtained by the character}
+#'  \item{character_pet_slots}{Data about the current battle pet slots on this characters account}
+#'  \item{character_professions}{A list of the character's professions. Does not include class professions}
+#'  \item{character_progression}{A list of raids and bosses indicating raid progression and completeness}
+#'  \item{character_pvp}{A map of pvp information including arena team membership and rated battlegrounds information}
+#'  \item{character_quests}{A list of quests completed by the character}
+#'  \item{character_reputation}{A list of the factions that the character has an associated reputation with}
+#'  \item{character_statistics}{A map of character statistics}
+#'  \item{character_stats}{A map of character attributes and stats}
+#'  \item{character_character_talents}{A list of talent structures}
+#'  \item{character_titles}{A list of the titles obtained by the character including the currently selected title}
+#'  \item{character_audit}{Raw character audit data that powers the character audit on the game site}
 #'
 #' }
 #'
 #' @export
 #' @source https://dev.battle.net/io-docs
-get_character_profile <- make_cp_query("")
+character_profile <- make_cp_query("")
 
 #' @export
-#' @rdname get_character_profile
-get_achievements <- make_cp_query("achievements")
+#' @rdname character_profile
+character_achievements <- make_cp_query("achievements")
 
 #' @export
-#' @rdname get_character_profile
-get_appearance <- make_cp_query("appearance")
+#' @rdname character_profile
+character_appearance <- make_cp_query("appearance")
 
 #' @export
-#' @rdname get_character_profile
-get_feed <- make_cp_query("feed")
+#' @rdname character_profile
+character_feed <- make_cp_query("feed")
 
 #' @export
-#' @rdname get_character_profile
-get_guild <- make_cp_query("guild")
+#' @rdname character_profile
+character_guild <- make_cp_query("guild")
 
 #' @export
-#' @rdname get_character_profile
-get_hunter_pets <- make_cp_query("hunterPets")
+#' @rdname character_profile
+character_hunter_pets <- make_cp_query("hunterPets")
 
 #' @export
-#' @rdname get_character_profile
-get_items <- make_cp_query("items")
+#' @rdname character_profile
+character_items <- make_cp_query("items")
 
 #' @export
-#' @rdname get_character_profile
-get_mounts <- make_cp_query("mounts")
+#' @rdname character_profile
+character_mounts <- make_cp_query("mounts")
 
 #' @export
-#' @rdname get_character_profile
-get_pets <- make_cp_query("pets")
+#' @rdname character_profile
+character_pets <- make_cp_query("pets")
 
 #' @export
-#' @rdname get_character_profile
-get_pet_slots <- make_cp_query("petSlots")
+#' @rdname character_profile
+character_pet_slots <- make_cp_query("petSlots")
 
 #' @export
-#' @rdname get_character_profile
-get_professions <- make_cp_query("professions")
+#' @rdname character_profile
+character_professions <- make_cp_query("professions")
 
 #' @export
-#' @rdname get_character_profile
-get_progression <- make_cp_query("progression")
+#' @rdname character_profile
+character_progression <- make_cp_query("progression")
 
 #' @export
-#' @rdname get_character_profile
-get_pvp <- make_cp_query("pvp")
+#' @rdname character_profile
+character_pvp <- make_cp_query("pvp")
 
 #' @export
-#' @rdname get_character_profile
-get_quests <- make_cp_query("quests")
+#' @rdname character_profile
+character_quests <- make_cp_query("quests")
 
 #' @export
-#' @rdname get_character_profile
-get_reputation <- make_cp_query("reputation")
+#' @rdname character_profile
+character_reputation <- make_cp_query("reputation")
 
 #' @export
-#' @rdname get_character_profile
-get_statistics <- make_cp_query("statistics")
+#' @rdname character_profile
+character_statistics <- make_cp_query("statistics")
 
 #' @export
-#' @rdname get_character_profile
-get_character_talents <- make_cp_query("talents")
+#' @rdname character_profile
+character_talents <- make_cp_query("talents")
 
 #' @export
-#' @rdname get_character_profile
-get_titles <- make_cp_query("titles")
+#' @rdname character_profile
+character_titles <- make_cp_query("titles")
 
 #' @export
-#' @rdname get_character_profile
-get_audit <- make_cp_query("audit")
+#' @rdname character_profile
+character_audit <- make_cp_query("audit")
 
--- R/data_resources.R
+++ R/data_resources.R
@@ -1,11 +1,6 @@
 data_resources <- function(subpath) {
-  baseurl <- "https://us.api.battle.net"
   path <- c("wow", "data", subpath)
-
-  query <- list(locale = "en_US",
-                apikey = Sys.getenv("WOW_API_KEY"))
-
-  httr::GET(baseurl, path = path,  query = query)
+  wow_api(path)
 }
 
 
@@ -13,54 +8,54 @@ data_resources <- function(subpath) {
 #'
 #' @details
 #' \describe{
-#'  \item{get_battlegroups}{The battlegroups data API provides the list of battlegroups for this region}
-#'  \item{get_character_races}{The character races data API provides a list of each race and their associated faction, name, unique ID, and skin}
-#'  \item{get_character_classes}{The character classes data API provides a list of character classes}
-#'  \item{get_character_achievements}{The character achievements data API provides a list of all of the achievements that characters can earn as well as the category structure and hierarchy}
-#'  \item{get_guild_rewards}{The guild rewards data API provides a list of all guild rewards}
-#'  \item{get_guild_perks}{The guild perks data API provides a list of all guild perks}
-#'  \item{get_guild_achievements}{The guild achievements data API provides a list of all of the achievements that guilds can earn as well as the category structure and hierarchy}
-#'  \item{get_item_classes}{The item classes data API provides a list of item classes}
-#'  \item{get_talents}{The talents data API provides a list of talents, specs and glyphs for each class}
-#'  \item{get_pet_types}{The different bat pet types (including what they are strong and weak against)}
+#'  \item{all_battlegroups}{The battlegroups data API provides the list of battlegroups for this region}
+#'  \item{all_character_races}{The character races data API provides a list of each race and their associated faction, name, unique ID, and skin}
+#'  \item{all_character_classes}{The character classes data API provides a list of character classes}
+#'  \item{all_character_achievements}{The character achievements data API provides a list of all of the achievements that characters can earn as well as the category structure and hierarchy}
+#'  \item{all_guild_rewards}{The guild rewards data API provides a list of all guild rewards}
+#'  \item{all_guild_perks}{The guild perks data API provides a list of all guild perks}
+#'  \item{all_guild_achievements}{The guild achievements data API provides a list of all of the achievements that guilds can earn as well as the category structure and hierarchy}
+#'  \item{all_item_classes}{The item classes data API provides a list of item classes}
+#'  \item{all_talents}{The talents data API provides a list of talents, specs and glyphs for each class}
+#'  \item{all_pet_types}{The different bat pet types (including what they are strong and weak against)}
 #'  }
 #'
 #' @export
 #' @source https://dev.battle.net/io-docs
-get_battlegroups <- function() data_resources("battlegroups/")
+all_battlegroups <- function() data_resources("battlegroups/")
 
 #' @export
-#' @rdname get_battlegroups
-get_character_races <- function() data_resources("character/races")
+#' @rdname all_battlegroups
+all_character_races <- function() data_resources("character/races")
 
 #' @export
-#' @rdname get_battlegroups
-get_character_classes <- function() data_resources("character/classes")
+#' @rdname all_battlegroups
+all_character_classes <- function() data_resources("character/classes")
 
 #' @export
-#' @rdname get_battlegroups
-get_character_achievements <- function() data_resources("character/achievements")
+#' @rdname all_battlegroups
+all_character_achievements <- function() data_resources("character/achievements")
 
 #' @export
-#' @rdname get_battlegroups
-get_guild_rewards <- function() data_resources("guild_rewards")
+#' @rdname all_battlegroups
+all_guild_rewards <- function() data_resources("guild_rewards")
 
 #' @export
-#' @rdname get_battlegroups
-get_guild_perks <- function() data_resources("guild_perks")
+#' @rdname all_battlegroups
+all_guild_perks <- function() data_resources("guild_perks")
 
 #' @export
-#' @rdname get_battlegroups
-get_guild_achievements <- function() data_resources("guild/achievements")
+#' @rdname all_battlegroups
+all_guild_achievements <- function() data_resources("guild/achievements")
 
 #' @export
-#' @rdname get_battlegroups
-get_item_classes <- function() data_resources("item/classes")
+#' @rdname all_battlegroups
+all_item_classes <- function() data_resources("item/classes")
 
 #' @export
-#' @rdname get_battlegroups
-get_talents <- function() data_resources("talents")
+#' @rdname all_battlegroups
+all_talents <- function() data_resources("talents")
 
 #' @export
-#' @rdname get_battlegroups
-get_pet_types <- function() data_resources("pet/types")
+#' @rdname all_battlegroups
+all_pet_types <- function() data_resources("pet/types")
