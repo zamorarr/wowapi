db0e58e88f207dedb657aa8d2b9b98dbb6239dd5
Robert Zamora <zamora.rr@gmail.com>
2016-10-01 00:41:24 -0400

    added download_auction helper function

--- R/auction.R
+++ R/auction.R
@@ -15,3 +15,23 @@ auction <- function(realm) {
   path <- c("wow", "auction", "data", realm)
   wow_api(path)
 }
+
+#' Download most recent auction data
+#'
+#' helper function to download auction data
+#'
+#' @param realm the name of the realm
+#' @param destpath the path to save the file to
+#'
+#' @export
+download_auction <- function(realm, destpath = tempdir()) {
+  auc <- auction(realm)
+  url <- auc$content$files[[1]]$url
+
+  timeval <- auc$content$files[[1]]$lastModified/1E3
+  destfile <- file.path(destpath, paste0("auctions_",realm,"_",timeval,".json"))
+
+  download.file(url, destfile)
+
+  destfile
+}
